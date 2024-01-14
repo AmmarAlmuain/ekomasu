a546c2c0c37fedd831e14942b4ac84cfb75b8fdd
AmmarAlmuain <masterofalune@gmail.com>
2024-01-14 16:23:02 +0300

    Responsive header and nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,8 +6,10 @@ export default function Header() {
   return (
     <>
       <header className="w-full h-20 py-0.5 bg-white border-b border-b-color">
-        <div className="full flex justify-between items-center shrink-sides">
-          <Logo />
+        <div className="full flex justify-between max-lg:gap-x-2 items-center max-xl:px-4 shrink-sides">
+          <div className="max-sm:hidden">
+            <Logo />
+          </div>
           <Search />
           <Actions />
         </div>
